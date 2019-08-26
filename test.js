ec7201456479bca4ca3a78dfbcaf4765a597fc76
Iordan Bogdanov <terrified@abv.bg>
2019-08-26 22:07:22 +0300

    added skeleton folder

--- test.js
+++ test.js
@@ -1,14 +1,34 @@
-function addSubtract(numOne, numTwo, numThree) {
-    let result = subtract(sum(numOne, numTwo), numThree);
-    console.log(result);
+function Solve(input) {
+    let linesNum = Number(input.shift());
+    let pattern = /^[\w\W]*([@*])(?<name>[A-Z][a-z]{2,})(\1): \[(?<ch1>[A-Za-z]{1})\]\|\[(?<ch2>[A-Za-z]{1})\]\|\[(?<ch3>[A-Za-z]{1})\]\|[ ]*$/g;
 
-    function sum(a, b) {
-        return a + b;
-    }
+    for (let i = 0; i < linesNum; i++) {
+        let currLine = pattern.exec(input[i]);
 
-    function subtract(a, b) {
-        return a - b;
+        if (currLine !== null) {
+            let name = currLine.groups.name;
+            let char1 = currLine.groups.ch1;
+            let char2 = currLine.groups.ch2;
+            let char3 = currLine.groups.ch3;
+
+            let decryptedChar1 = char1.charCodeAt(0);
+            let decryptedChar2 = char2.charCodeAt(0);
+            let decryptedChar3 = char3.charCodeAt(0);
+
+            console.log(`${name}: ${decryptedChar1} ${decryptedChar2} ${decryptedChar3}`);
+        } else {
+            console.log('Valid message not found!');
+        }
     }
 }
 
-addSubtract(23, 6, 10);
+Solve(['3',
+    '*Request*: [I]|[b]|[i]|',
+    '*Taggy@: [73]|[73]|[73]|',
+    'Should be valid @Taggy@: [v]|[a]|[l]|'
+]);
+Solve(['3',
+    '@Taggy@: [i]|[n]|[v]|[a]|[l]|[i]|[d]| this shouldn�t be valid',
+    '*tAGged*: [i][i][i]|',
+    'Should be invalid @Taggy@: [v]|[a]|[l]|[l]|[l]|'
+]);
--- test2.js
+++ test2.js
@@ -1,35 +1,13 @@
-function Solve(rows, cols) {
-    let matrix = createMatrix();
-    fillMatrix();
+function Solve(input) {
+    let pattern = /:(?<words>[a-z]{4,}):[,.!?]*/g;
+    let emojies = pattern.exec(input[0]);
 
-    console.log(matrix.map(row => row.join(' ')).join('\n'));
-
-    function fillMatrix(currentValue = 1, startIndex = 0) {
-        for (let i = startIndex; i < matrix[startIndex].length - startIndex; i++) {
-            matrix[startIndex][i] = currentValue++;
-        }
-        for (let i = startIndex + 1; i < matrix.length - startIndex; i++) {
-            matrix[i][matrix[i].length - 1 - startIndex] = currentValue++;
-        }
-        for (let i = matrix.length - 2 - startIndex; i > startIndex; i--) {
-            matrix[matrix.length - 1 - startIndex][i] = currentValue++;
-        }
-        for (let i = matrix.length - 1 - startIndex; i > startIndex; i--) {
-            matrix[i][startIndex] = currentValue++;
-        }
-        if (currentValue <= matrix.length * matrix[0].length) {
-            startIndex++;
-            fillMatrix(currentValue, startIndex);
-        }
-    }
-
-    function createMatrix() {
-        let matrix = [];
-        for (let i = 0; i < rows; i++) {
-            matrix.push(new Array(cols));
-        }
-        return matrix;
+    while (emojies !== null) {
+        console.log(emojies.groups);
+        emojies = pattern.exec(input[0]);
     }
 }
 
-Solve(5, 5);
+Solve(['Hello I am Mark from :England: and I am :one: :seven: years old, I have a :hamster: as pet.',
+    '115:101:118:101:110'
+]);
